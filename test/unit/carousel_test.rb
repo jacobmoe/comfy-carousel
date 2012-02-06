a9597c3a6d20ff9ef1e6d3725e749e2c7a2e0f64
Oleg <oleg@theworkinggroup.ca>
2012-02-06 13:24:45 -0500

    unit tests for carousel

--- test/unit/carousel_test.rb
+++ test/unit/carousel_test.rb
@@ -3,19 +3,28 @@ require File.expand_path('../test_helper', File.dirname(__FILE__))
 class CarouselTest < ActiveSupport::TestCase
   
   def test_fixtures_validity
-    
+    Carousel::Carousel.all.each do |carousel|
+      assert carousel.valid?, carousel.errors.inspect 
+    end
   end
   
   def test_validations
-    
+    carousel = Carousel::Carousel.new
+    assert carousel.invalid?
+    assert_has_errors_on carousel, [:label, :identifier]
   end
   
   def test_creation
-    
+    assert_difference 'Carousel::Carousel.count' do
+      carousel = Carousel::Carousel.create(:identifier => 'test')
+      assert_equal 'Test', carousel.label
+    end
   end
   
   def test_destruction
-    
+    assert_difference ['Carousel::Carousel.count', 'Carousel::Slide.count'], -1 do
+      carousel_carousels(:default).destroy
+    end
   end
   
 end
