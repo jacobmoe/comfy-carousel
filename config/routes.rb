739f0ddc0f7c1ba55e2d8b85872bda17f2244be1
Jacob Moeller <jacobmoe@gmail.com>
2013-06-26 14:48:51 -0400

    rolling back

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   
-  
-  ComfortableMexicanSofa::Routing.admin(:path => '/admin')
-  
-  # Make sure this routeset is defined last
-  ComfortableMexicanSofa::Routing.content(:path => '/', :sitemap => false)
   namespace :admin, :path => ComfyCarousel.config.admin_route_prefix do
     namespace :carousel do
       resources :carousels, :except => [:show] do
